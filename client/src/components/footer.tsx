3079bcf212c25cc6e3d2e83db52001e64d395b5c
muchen100 <65653038+muchen100@users.noreply.github.com>
2025-04-29 14:22:12 +0800

    Update footer.tsx

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -139,4 +139,35 @@ function ThemeButton({ current, mode, label, icon, onClick }: { current: ThemeMo
     </button>)
 }
 
+
+
+
+
+
+
+
+
+
+
+function hitokoto(o) {
+    $("#hitokoto").stop().fadeOut((function() {
+        $("#hitokoto").html(o.hitokoto), document.getElementById("hitokotoa").href = "https://hitokoto.cn/?uuid=" + o.uuid, $("#hitokoto").stop().fadeIn()
+    }))
+}
+
+function getHitokoto() {
+    var o = ["a", "b", "c", "d", "e", "i"];
+    fetch("https://v1.hitokoto.cn/?encode=json&charset=utf-8&c=" + o[Math.floor(Math.random() * o.length)], {
+        cache: "no-cache",
+        method: "GET",
+        mode: "cors"
+    }).then(o => o.json()).then(o => {
+        hitokoto(o), setTimeout(getHitokoto, 5e3)
+    }).catch(console.error)
+}
+$(document).ready((function() {
+    getHitokoto()
+}))
+
+
 export default Footer;
